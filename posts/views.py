641139a5812c6e38c21324e69730d4bf527b53e2
gofur <gofur@Admins-iMac.local>
2016-01-14 17:17:26 +0700

    second commit

---
+++ posts/views.py
@@ -0,0 +1,36 @@
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+# Create your views here.
+from .models import Post
+
+def post_create(request):
+	return HttpResponse("<h1>Create</h1>")
+
+def post_detail(request):
+	# instance = Post.objects.get(id=1)
+	instance = get_object_or_404(Post,id=3)
+	context = {
+		"title": instance.title,
+		"instance": instance,
+	}
+	return render(request, "post_detail.html", context)
+
+def post_list(request):
+	queryset = Post.objects.all()
+	if request.user.is_authenticated():
+		context = {
+			"object_list": queryset,
+			"title": "List"
+			}
+	else:
+		context = {
+			"title": "List"
+		}
+	return render(request, "index.html", context)
+
+def post_update(request):
+	return HttpResponse("<h1>Update</h1>")
+
+def post_delete(request):
+	return HttpResponse("<h1>Delete</h1>")
