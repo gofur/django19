eaef0ebdd6ff7e29da42d5300480a27d75694c52
Your Name <you@example.com>
2016-01-18 12:25:17 +0700

    update source

--- posts/models.py
+++ posts/models.py
@@ -20,3 +20,8 @@ class Post(models.Model):
 
 	def get_absolute_url(self):
 		return reverse("posts:detail", kwargs={"id_detail": self.id})
+	
+	class Meta:
+		ordering = ["-timestamp", "-updated"]
+			
+			
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
 
@@ -31,17 +32,31 @@ def post_detail(request, id_detail=None): #retrieve
 	return render(request, "post_detail.html", context)
 
 def post_list(request):
-	queryset = Post.objects.all()
+	queryset_list = Post.objects.all() #.order_by("-timestamp")
+	paginator = Paginator(queryset_list, 25) # Show 25 contacts per page
+
+	page = request.GET.get('page')
+	try:
+		queryset = paginator.page(page)
+	except PageNotAnInteger:
+	# If page is not an integer, deliver first page.
+		queryset = paginator.page(1)
+	except EmptyPage:
+	# If page is out of range (e.g. 9999), deliver last page of results.
+		queryset = paginator.page(paginator.num_pages)
+
+
 	if request.user.is_authenticated():
 		context = {
 			"object_list": queryset,
 			"title": "List"
-			}
+		}
 	else:
 		context = {
 			"title": "List"
 		}
 	return render(request, "post_list.html", context)
+
 
 def post_update(request, id_detail=None):
 	instance = get_object_or_404(Post,id=id_detail)
