c6506384d76d02838149dbf2c6dbf17f167dd4a3
Your Name <you@example.com>
2016-01-19 07:10:34 +0700

    update source

--- posts/models.py
+++ posts/models.py
@@ -13,16 +13,12 @@ def upload_location(instance, filename):
 
 class Post(models.Model):
 	title = models.CharField(max_length=120)
-<<<<<<< HEAD
 	image = models.ImageField(upload_to=upload_location, null=True, 
             blank=True, 
             width_field="width_field", 
             height_field="height_field")
 	height_field = models.IntegerField(default=0)
 	width_field = models.IntegerField(default=0)
-=======
-	image = models.FileField(null=True, blank=True)
->>>>>>> 8b7f2e1b7f52077882ddf9a71b3bbf4255dd71b1
 	content =  models.TextField()
 	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 	timestamp = models.DateTimeField(auto_now=False,auto_now_add=True)
